fcb228ec4fc305de5ed02dbf9308e2abd152eb7b
Jusehun <aaawind1234@gmail.com>
2025-07-04 10:04:47 +0900

    feat: modify calulator3

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
 public class Calculator  {
-    public int add(String str1, String str2) {
-        int b = Integer.parseInt(str1);
-        int c = Integer.parseInt(str2);
-        return b + c;
+    public long add(String str1, String str2) {
+        int a = Integer.parseInt(str1);
+        int b = Integer.parseInt(str2);
+        int longA = a;
+        int longB = b;
+        return a + b;
     }
 }
